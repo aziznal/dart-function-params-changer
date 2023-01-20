b6823bf05a4e310a592e5bef7c4be0a492760d88
Aziz <aziznal.dev@gmail.com>
2023-01-20 20:56:12 +0300

    Setup parser for parsing position, named, and optional params

--- src/core/parser/parser.test.ts
+++ src/core/parser/parser.test.ts
@@ -1,4 +1,9 @@
-import { Parser, ParserImpl, FunctionDefinition } from './parser';
+import {
+    Parser,
+    ParserImpl,
+    FunctionDefinition,
+    FunctionParam,
+} from './parser';
 
 describe('Parser', () => {
     let parser: Parser;
@@ -9,42 +14,235 @@ describe('Parser', () => {
 
     // NOTE: this test is sensitive to tabs and spaces
     it('Parses a basic function definition', () => {
-        expect(parser.parseFunctionFromRaw(`void foo() {}`)).toEqual(
+        // most basic form
+        expect(parser.parseFunction(`foo() {}`)).toEqual(
             new FunctionDefinition('foo', [])
         );
 
-        expect(parser.parseFunctionFromRaw(`foo() {}`)).toEqual(
+        // with return type
+        expect(parser.parseFunction(`void foo() {}`)).toEqual(
             new FunctionDefinition('foo', [])
         );
 
-        expect(parser.parseFunctionFromRaw(`foo1() {}`)).toEqual(
+        // with number in name
+        expect(parser.parseFunction(`foo1() {}`)).toEqual(
             new FunctionDefinition('foo1', [])
         );
 
-        expect(parser.parseFunctionFromRaw(`Future<void> foo1() {}`)).toEqual(
+        // with more complex return type
+        expect(parser.parseFunction(`Future<void> foo1() {}`)).toEqual(
             new FunctionDefinition('foo1', [])
         );
 
+        // with long name
         expect(
-            parser.parseFunctionFromRaw(
-                `Future<void> functionWithALongName() {}`
-            )
+            parser.parseFunction(`Future<void> functionWithALongName() {}`)
         ).toEqual(new FunctionDefinition('functionWithALongName', []));
 
+        // multiline with tabs
         expect(
-            parser.parseFunctionFromRaw(
+            parser.parseFunction(
                 `Future<void> foo(
 
                 ) {}`
             )
         ).toEqual(new FunctionDefinition('foo', []));
 
+        // multiline with tabs in weird place
         expect(
-            parser.parseFunctionFromRaw(
+            parser.parseFunction(
                 `void foo   (   
 
                 ) {}`
             )
         ).toEqual(new FunctionDefinition('foo', []));
+    });
+
+    it('Throws an error when no function could be parsed', () => {
+        expect(() => parser.parseFunction('')).toThrowError();
+
+        expect(() => parser.parseFunction('void')).toThrowError();
+
+        expect(() => parser.parseFunction('foo')).toThrowError();
+
+        expect(() => parser.parseFunction('foo {}')).toThrowError();
+
+        expect(() => parser.parseFunction('()')).toThrowError();
+
+        expect(() => parser.parseFunction('() {}')).toThrowError();
+
+        expect(() => parser.parseFunction('   () {}')).toThrowError();
+    });
+
+    it("Parses a function's positional params", () => {
+        // no params
+        expect(parser.parseFunction('foo() {}')).toEqual(
+            new FunctionDefinition('foo', [])
+        );
+
+        // with one basic param
+        expect(parser.parseFunction('foo(int param1) {}')).toEqual(
+            new FunctionDefinition('foo', [new FunctionParam('param1', 'int')])
+        );
+
+        // with two basic params
+        expect(parser.parseFunction('foo(int param1, int param2) {}')).toEqual(
+            new FunctionDefinition('foo', [
+                new FunctionParam('param1', 'int'),
+                new FunctionParam('param2', 'int'),
+            ])
+        );
+
+        // with two basic params and a trailing comma
+        expect(parser.parseFunction('foo(int param1, int param2,) {}')).toEqual(
+            new FunctionDefinition('foo', [
+                new FunctionParam('param1', 'int'),
+                new FunctionParam('param2', 'int'),
+            ])
+        );
+
+        // with three basic params
+        expect(
+            parser.parseFunction('foo(int param1, int param2, int param3) {}')
+        ).toEqual(
+            new FunctionDefinition('foo', [
+                new FunctionParam('param1', 'int'),
+                new FunctionParam('param2', 'int'),
+                new FunctionParam('param3', 'int'),
+            ])
+        );
+
+        // with three params of different types
+        expect(
+            parser.parseFunction(
+                'foo(string param1, bool param2, int param3) {}'
+            )
+        ).toEqual(
+            new FunctionDefinition('foo', [
+                new FunctionParam('param1', 'string'),
+                new FunctionParam('param2', 'bool'),
+                new FunctionParam('param3', 'int'),
+            ])
+        );
+
+        // with three params of different types, badly formatted
+        expect(
+            parser.parseFunction(
+                'foo (          string           param1,              bool param2,     int param3         )     {}'
+            )
+        ).toEqual(
+            new FunctionDefinition('foo', [
+                new FunctionParam('param1', 'string'),
+                new FunctionParam('param2', 'bool'),
+                new FunctionParam('param3', 'int'),
+            ])
+        );
+
+        // with one optional param
+        expect(
+            parser.parseFunction('foo(int param1, int param2, [int foo=1]) {}')
+        ).toEqual(
+            new FunctionDefinition('foo', [
+                new FunctionParam('param1', 'int'),
+                new FunctionParam('param2', 'int'),
+                new FunctionParam('foo', 'int', '1'),
+            ])
+        );
+
+        // with multiple optional params
+        expect(
+            parser.parseFunction(
+                'foo(int param1, int param2, [int foo=1, string foo2="2"]) {}'
+            )
+        ).toEqual(
+            new FunctionDefinition('foo', [
+                new FunctionParam('param1', 'int'),
+                new FunctionParam('param2', 'int'),
+                new FunctionParam('foo', 'int', '1'),
+                new FunctionParam('foo2', 'string', '"2"'),
+            ])
+        );
+
+        // with optional params without a type
+        expect(
+            parser.parseFunction(
+                'foo(int param1, int param2, [foo=1, foo2="2"]) {}'
+            )
+        ).toEqual(
+            new FunctionDefinition('foo', [
+                new FunctionParam('param1', 'int'),
+                new FunctionParam('param2', 'int'),
+                new FunctionParam('foo', undefined, '1'),
+                new FunctionParam('foo2', undefined, '"2"'),
+            ])
+        );
+    });
+
+    // prettier-ignore
+    it("Parses a function's named params", () => {
+        // with one param
+        expect(parser.parseFunction('foo({int param1}) {}')).toEqual(
+            new FunctionDefinition(
+                'foo',
+                [
+                    new FunctionParam('param1', 'int')
+                ]
+            )
+        );
+
+        // with two params
+        expect(parser.parseFunction('foo({int param1, string param2}) {}')).toEqual(
+            new FunctionDefinition(
+                'foo',
+                [
+                    new FunctionParam('param1', 'int'),
+                    new FunctionParam('param2', 'string'),
+                ]
+            )
+        );
+
+        // with two params and a trailing comma
+        expect(parser.parseFunction('foo({int param1, string param2,}) {}')).toEqual(
+            new FunctionDefinition(
+                'foo',
+                [
+                    new FunctionParam('param1', 'int'),
+                    new FunctionParam('param2', 'string'),
+                ]
+            )
+        );
+
+        // with two params prefixed by 'required' keyword
+        expect(parser.parseFunction('foo({required int param1, required string param2,}) {}')).toEqual(
+            new FunctionDefinition(
+                'foo',
+                [
+                    new FunctionParam('param1', 'int'),
+                    new FunctionParam('param2', 'string'),
+                ]
+            )
+        );
+
+        // with two params with default values (with types)
+        expect(parser.parseFunction('foo({int param1=3, string param2="3",}) {}')).toEqual(
+            new FunctionDefinition(
+                'foo',
+                [
+                    new FunctionParam('param1', 'int', "3"),
+                    new FunctionParam('param2', 'string', '"3"'),
+                ]
+            )
+        );
+
+        // with two params with default values (without types)
+        expect(parser.parseFunction('foo({param1=3, param2="3",}) {}')).toEqual(
+            new FunctionDefinition(
+                'foo',
+                [
+                    new FunctionParam('param1', undefined, "3"),
+                    new FunctionParam('param2', undefined, '"3"'),
+                ]
+            )
+        );
     });
 });
--- src/core/parser/parser.ts
+++ src/core/parser/parser.ts
@@ -1,25 +1,21 @@
-class FunctionParam {
+export class FunctionParam {
     constructor(
         public name: string,
-        public type: string,
+        public type?: string,
         public defaultValue?: string
     ) {}
 }
 
 export class FunctionDefinition {
-    constructor(
-        public functionName: string,
-
-        public params: FunctionParam[]
-    ) {}
+    constructor(public functionName: string, public params: FunctionParam[]) {}
 }
 
 export abstract class Parser {
-    abstract parseFunctionFromRaw(raw: string): FunctionDefinition;
+    abstract parseFunction(raw: string): FunctionDefinition;
 }
 
 export class ParserImpl implements Parser {
-    parseFunctionFromRaw(raw: string): FunctionDefinition {
+    parseFunction(raw: string): FunctionDefinition {
         const regex =
             /(?<functionName>\w+)\t*[ ]*\([ ]*[\r\n]?\t*[ ]*(?<functionParams>.*)[\r\n]?.*\)/;
 
@@ -29,8 +25,122 @@ export class ParserImpl implements Parser {
             throw Error('could not parse function');
         }
 
-        const [_, functionName, functionParams] = extracted;
+        const [_, functionName, rawFunctionParams] = extracted;
 
-        return new FunctionDefinition(functionName, []);
+        const parsedParams = this.#parseParams(rawFunctionParams);
+
+        return new FunctionDefinition(functionName, parsedParams);
+    }
+
+    #parseParams(rawParams: string): FunctionParam[] {
+        const paramsType = this.#detectParamsType(rawParams);
+
+        if (paramsType === 'positional') {
+            return this.#parsePositionalParams(rawParams);
+        }
+
+        if (paramsType === 'positional-with-optional') {
+            return this.#parsePositionalParams(rawParams).concat(
+                this.#parseOptionalParams(rawParams)
+            );
+        }
+
+        return this.#parseNamedParams(rawParams);
+    }
+
+    #detectParamsType(
+        rawParams: string
+    ): 'positional' | 'positional-with-optional' | 'named' {
+        if (rawParams.includes('{')) {
+            return 'named';
+        }
+
+        if (rawParams.includes('[')) {
+            return 'positional-with-optional';
+        }
+
+        return 'positional';
+    }
+
+    #parsePositionalParams(rawParams: string): FunctionParam[] {
+        if (rawParams.length === 0) {
+            return [];
+        }
+
+        return rawParams
+            .replace(/\[.*/g, '') // remove any optional params
+            .trim()
+            .replace(/ +(?= )/g, '') // replace multiple whitespaces with only one
+            .split(',')
+            .filter((param) => param.length > 0) // to account for trailing commas
+            .map((param) => param.trim())
+            .map((param) => param.split(' '))
+            .map(
+                (splitParam) => new FunctionParam(splitParam[1], splitParam[0])
+            );
+    }
+
+    #parseOptionalParams(rawParams: string): FunctionParam[] {
+        if (rawParams.length === 0) {
+            return [];
+        }
+
+        return rawParams
+            .replace(/.*\[/g, '') // remove everything before the optional params
+            .replace(/\]/g, '')
+            .trim()
+            .replace(/ +(?= )/g, '') // replace multiple whitespaces with only one
+            .split(',')
+            .filter((param) => param.length > 0) // to account for trailing commas
+            .map((param) => param.trim())
+            .map((param) => param.split(' ')) // get type-name pairs
+            .map((typeNamePair) => {
+                // if no type given
+                if (typeNamePair.length === 1) {
+                    return new FunctionParam(
+                        typeNamePair[0].split('=')[0],
+                        undefined,
+                        typeNamePair[0].split('=')[1]
+                    );
+                }
+
+                return new FunctionParam(
+                    typeNamePair[1].split('=')[0],
+                    typeNamePair[0],
+                    typeNamePair[1].split('=')[1]
+                );
+            });
+    }
+
+    #parseNamedParams(rawParams: string): FunctionParam[] {
+        if (rawParams.length === 0) {
+            return [];
+        }
+
+        return rawParams
+            .replace(/{|}/g, '') // remove curly braces
+            .replace(/required /g, '')
+            .trim()
+            .replace(/ +(?= )/g, '') // replace multiple whitespaces with only one
+            .split(',')
+            .filter((param) => param.length > 0) // to account for trailing commas
+            .map((param) => param.trim())
+            .map((param) => param.split(' '))
+            .map((typeNamePair) => {
+                // if no type given
+                if (typeNamePair.length === 1) {
+                    return new FunctionParam(
+                        typeNamePair[0].split('=')[0],
+                        undefined,
+                        typeNamePair[0].split('=')[1]
+                    );
+                }
+
+                return new FunctionParam(
+                    typeNamePair[1].split('=')[0],
+                    typeNamePair[0],
+                    typeNamePair[1].split('=')[1]
+                );
+            });
     }
 }
