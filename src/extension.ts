763b5a4d961c11294a02fb912c09465c8b86e668
Aziz <aziznal.dev@gmail.com>
2023-01-23 09:18:17 +0300

    Format code in entry file

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,16 @@
-
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('dart-function-params-changer.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from Dart Function Params Changer!');
-	});
+    let disposable = vscode.commands.registerCommand(
+        'dart-function-params-changer.helloWorld',
+        () => {
+            vscode.window.showInformationMessage(
+                'Hello World from Dart Function Params Changer!'
+            );
+        }
+    );
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
